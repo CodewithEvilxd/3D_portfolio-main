43c18ed8a481bc4b8b767620993ab3420a873e0a
nishantgauravhere <126612139+CodewithEvilxd@users.noreply.github.com>
2024-04-12 03:21:14 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,228 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    {
+        title: " Developer",
+        company_name: "code",
+        icon: starbucks,
+        iconBg: "#accbe1",
+        date: " 2019 -  2021",
+        points: [
+            "I Started my coding journy From Telegram.",
+            "I learned Python Because I love to MAke Telegram Bot.",
+            "Then I Deside To Lern Java C# and C For Machine Learning.",
+            
+        ],
+    },
+    {
+        title: "React Native Developer",
+        company_name: "AFTER TELEGRAM",
+        icon: tesla,
+        iconBg: "#fbc3bc",
+        date: " 2021 -2022",
+        points: [
+            "I Learned Many Machine Learning Language Like C, C++ Also I Learn About JAVA.",
+            "At That Time I Had Not Laptop, I learned Thats ALL Language From Phone",
+            "In The ENding Of 2022, I Desided To Learn ABout WEB DEVELOPIG .",
+            
+        ],
+    },
+    {
+        title: "Web Developer",
+        company_name: "After !0th",
+        icon: shopify,
+        iconBg: "#b7e4c7",
+        date: " 2022 - 2024",
+        points: [
+            "When I come Out OF home To STudy In Patna I Learned Javascript Bootstrap Reactjs Nodejs MangooDB SQL ",
+            "Then I Started To DO Freelancing .",
+            "I Also Started To Participate In Many COmpidition",
+        
+        ],
+    },
+    {
+        title: "Full stack Developer",
+        company_name: "IN 11th And 12th",
+        icon: meta,
+        iconBg: "#a2d2ff",
+        date: " 2023 - Present",
+        points: [
+            "I learned Full Front ANd Backdevelopment ALso i Started to Learn Game Development.",
+            "I Desided To Create Many Project ANd I Created That",
+            "CUrrentlly I am Im Improving mySelf To BE Best",
+        
+        ],
+    },
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/codewithevilxd',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/YourLinkedInUsername',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'Amazon Price Tracker',
+        description: 'Developed a web application that tracks and notifies users of price changes for products on Amazon, helping users find the best deals.',
+        link: 'https://github.com/codewithevilxd/pricewise',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'Full Stack Threads Clone',
+        description: 'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
+        link: 'https://github.com/codewithevilxd/threads',
+    },
+    {
+        iconUrl: car,
+        theme: 'btn-back-blue',
+        name: 'Car Finding App',
+        description: 'Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.',
+        link: 'https://github.com/codewithevilxd/project_next13_car_showcase',
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: 'Full Stack Instagram Clone',
+        description: 'Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.',
+        link: 'https://github.com/codewithevilxd/social_media_app',
+    },
+    {
+        iconUrl: estate,
+        theme: 'btn-back-black',
+        name: 'Real-Estate Application',
+        description: 'Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.',
+        link: 'https://github.com/codewithevilxd/projects_realestate',
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'AI Summarizer Application',
+        description: 'App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.',
+        link: 'https://github.com/codewithevilxd/project_ai_summarizer',
+    }
+];
